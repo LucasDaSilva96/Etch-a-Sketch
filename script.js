9f518b8b4e75eb60d1f9c52a89da3104f5b0b5d6
LucasDaSilva96 <dasilvajunior881@gmail.com>
2023-05-17 19:13:21 +0200

    Created the grid_range function to display the current amount of grid chosen.

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 "use strict";
+
+// DOM selection
+const grid_range = document.getElementById("range-slider");
+const grid_range_text = document.querySelector(".range-nr");
+
+// Display the current grid-value on change
+grid_range.addEventListener("click", function () {
+  grid_range_text.textContent = `${grid_range.value} x ${grid_range.value}`;
+});
